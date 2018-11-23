bfe8298c97d69d05b203bf28706842bbbee9098f
Aaryatech <code@atsinfosoft.com>
2018-11-23 11:05:07 +0530

    orApi

--- src/main/java/com/shivshambhuwebapi/controller/OrderApiController.java
+++ src/main/java/com/shivshambhuwebapi/controller/OrderApiController.java
@@ -61,5 +61,14 @@ public class OrderApiController {
 		return orderRes;
 
 	}
+	/*
+	 
+	 SELECT t_order_header.*,
+m_plant.plant_name,m_company.comp_name,m_customer.cust_name,m_project.proj_name,m_project.address
+FROM t_order_header,m_plant,m_company,m_customer,m_project
+WHERE t_order_header.plant_id=m_plant.plant_id AND m_plant.company_id=m_company.company_id AND t_order_header.cust_id=m_customer.cust_id AND m_project.proj_id=t_order_header.proj_id
+AND t_order_header.plant_id=:plantId AND t_order_header.cust_id=:custId
+t_order_header.order_date BETWEEN :fromDate AND :toDate  ORDER BY t_order_header.order_date DESC 
+	 */
 
 }
