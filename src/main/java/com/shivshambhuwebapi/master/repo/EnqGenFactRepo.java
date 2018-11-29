3a7221f5d24a327b79ec701ebff8d4e120911a29
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2018-11-29 18:23:33 +0530

    // neha

--- src/main/java/com/shivshambhuwebapi/master/repo/EnqGenFactRepo.java
+++ src/main/java/com/shivshambhuwebapi/master/repo/EnqGenFactRepo.java
@@ -25,4 +25,6 @@ public interface EnqGenFactRepo extends JpaRepository<EnqGenFact, Integer> {
 	@Query("UPDATE EnqGenFact SET delStatus=0  WHERE enqGenId IN(:enqGenIds)")
 	int deleteMultiEnqGenFact(@Param("enqGenIds") List<Integer> enqGenIds);
 
+	EnqGenFact findByEnqGenByAndDelStatus(String enqGenBy, int i);
+
 }
