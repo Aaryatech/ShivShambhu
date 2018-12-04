5a2b35ec497daf0fb3d3eac885d39326f997d481
Aaryatech <code@atsinfosoft.com>
2018-12-04 14:47:18 +0530

    po..sac

--- src/main/java/com/shivshambhuwebapi/tx/repo/GetItemWithEnqRepo.java
+++ src/main/java/com/shivshambhuwebapi/tx/repo/GetItemWithEnqRepo.java
@@ -27,20 +27,20 @@ public interface GetItemWithEnqRepo extends JpaRepository<GetItemWithEnq, Intege
 			" COALESCE((SELECT t_quot_detail.other_cost from t_quot_detail,t_quot_header "
 			+ " WHERE t_quot_detail.quot_head_id=t_quot_header.quot_head_id AND t_quot_header.enq_head_id=:enqHeadId "
 			+ " AND t_quot_detail.item_id=i.item_id),0) AS other_cost," +
-			
+
 			//
-			
-" COALESCE((SELECT t_quot_detail.tax_value from t_quot_detail,t_quot_header "
-+ " WHERE t_quot_detail.quot_head_id=t_quot_header.quot_head_id AND t_quot_header.enq_head_id=:enqHeadId "
-+ " AND t_quot_detail.item_id=i.item_id),0) AS tax_value," +
 
-" COALESCE((SELECT t_quot_detail.taxable_value from t_quot_detail,t_quot_header "
-+ " WHERE t_quot_detail.quot_head_id=t_quot_header.quot_head_id AND t_quot_header.enq_head_id=:enqHeadId "
-+ " AND t_quot_detail.item_id=i.item_id),0) AS taxable_value," +
+			" COALESCE((SELECT t_quot_detail.tax_value from t_quot_detail,t_quot_header "
+			+ " WHERE t_quot_detail.quot_head_id=t_quot_header.quot_head_id AND t_quot_header.enq_head_id=:enqHeadId "
+			+ " AND t_quot_detail.item_id=i.item_id),0) AS tax_value," +
 
-" COALESCE((SELECT t_quot_detail.total from t_quot_detail,t_quot_header "
-+ " WHERE t_quot_detail.quot_head_id=t_quot_header.quot_head_id AND t_quot_header.enq_head_id=:enqHeadId "
-+ " AND t_quot_detail.item_id=i.item_id),0) AS final_total," +
+			" COALESCE((SELECT t_quot_detail.taxable_value from t_quot_detail,t_quot_header "
+			+ " WHERE t_quot_detail.quot_head_id=t_quot_header.quot_head_id AND t_quot_header.enq_head_id=:enqHeadId "
+			+ " AND t_quot_detail.item_id=i.item_id),0) AS taxable_value," +
+
+			" COALESCE((SELECT t_quot_detail.total from t_quot_detail,t_quot_header "
+			+ " WHERE t_quot_detail.quot_head_id=t_quot_header.quot_head_id AND t_quot_header.enq_head_id=:enqHeadId "
+			+ " AND t_quot_detail.item_id=i.item_id),0) AS final_total," +
 
 			//
 
@@ -56,7 +56,7 @@ public interface GetItemWithEnqRepo extends JpaRepository<GetItemWithEnq, Intege
 			+ " WHERE t_enq_detail.enq_head_id=t_enq_header.enq_head_id AND t_enq_header.plant_id=p.plant_id "
 			+ " AND t_enq_header.enq_head_id=:enqHeadId AND t_enq_detail.item_id=i.item_id AND t_enq_detail.enq_uom_id=m_uom.uom_id),'NA') AS enq_uom_name"
 
-			+ " FROM m_item i,m_plant p ,m_tax t,m_uom u "
+			+ " FROM m_item_fg i,m_plant p ,m_tax t,m_uom u "
 			+ " WHERE i.del_status=1 AND p.plant_id=i.plant_id AND p.plant_id=:plantId AND t.tax_id=i.tax_id AND u.uom_id=i.uom_id"
 			+ "  ", nativeQuery = true)
 
--- src/main/java/com/shivshambhuwebapi/tx/repo/GetPoForOrderRepo.java
+++ src/main/java/com/shivshambhuwebapi/tx/repo/GetPoForOrderRepo.java
@@ -15,12 +15,12 @@ public interface GetPoForOrderRepo extends JpaRepository<GetPoForOrder, Integer>
 			+ " t_cust_po_detail.po_remaining_qty,t_cust_po_detail.status,t_cust_po_detail.remark,"
 			+ " t_cust_po_detail.tax_amt,t_cust_po_detail.tax_per,t_cust_po_detail.taxable_amt,"
 			+ " t_cust_po_detail.other_charges,t_cust_po_detail.total,t_cust_po_detail.qu_detail_id, "
-			+ " t_cust_po_header.po_id,t_cust_po_header.po_no,t_cust_po_header.po_date, m_item.item_code,"
-			+ " m_item.item_name,m_item.short_name,m_item.dispatch_limit,m_item.royalty_rate,"
-			+ " m_item.freight_rate,m_item.item_rate1, m_uom.uom_name "
-			+ " FROM t_cust_po_detail,t_cust_po_header,m_item,m_uom "
+			+ " t_cust_po_header.po_id,t_cust_po_header.po_no,t_cust_po_header.po_date, m_item_fg.item_code,"
+			+ " m_item_fg.item_name,m_item_fg.short_name,m_item_fg.dispatch_limit,m_item_fg.royalty_rate,"
+			+ " m_item_fg.freight_rate,m_item_fg.item_rate1, m_uom.uom_name "
+			+ " FROM t_cust_po_detail,t_cust_po_header,m_item_fg,m_uom "
 			+ " WHERE t_cust_po_detail.po_id=t_cust_po_header.po_id AND"
-			+ " t_cust_po_detail.item_id=m_item.item_id AND m_item.uom_id=m_uom.uom_id "
+			+ " t_cust_po_detail.item_id=m_item_fg.item_id AND m_item_fg.uom_id=m_uom.uom_id "
 			+ " AND t_cust_po_header.po_id=:poId ", nativeQuery = true)
 	
 	List<GetPoForOrder> getGetPoForOrderByPoId(@Param("poId") int poId);
