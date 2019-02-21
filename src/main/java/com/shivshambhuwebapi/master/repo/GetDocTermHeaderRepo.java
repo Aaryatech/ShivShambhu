aaa0a1a5cd4ae2651760f7f554b27fd290b8c432
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-02-21 13:06:46 +0530

    //neha

--- src/main/java/com/shivshambhuwebapi/master/repo/GetDocTermHeaderRepo.java
+++ src/main/java/com/shivshambhuwebapi/master/repo/GetDocTermHeaderRepo.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.shivshambhuwebapi.master.model.GetDocTermHeader;
 
@@ -11,5 +12,8 @@ public interface GetDocTermHeaderRepo extends JpaRepository<GetDocTermHeader, In
 
 	@Query(value = "SELECT h.*,d.doc_name FROM doc_term_header h,m_document d WHERE h.del_status=1 AND d.doc_id=h.doc_id  ORDER BY h.term_id DESC", nativeQuery = true)
 	List<GetDocTermHeader> getDocHeaderList();
+
+	@Query(value = "SELECT h.*,d.doc_name FROM doc_term_header h,m_document d WHERE h.del_status=1 AND d.doc_id=h.doc_id  AND h.ex_int1=:plantId ORDER BY h.term_id DESC", nativeQuery = true)
+	List<GetDocTermHeader> getDocHeaderListByPlantId(@Param("plantId") int plantId);
 
 }
