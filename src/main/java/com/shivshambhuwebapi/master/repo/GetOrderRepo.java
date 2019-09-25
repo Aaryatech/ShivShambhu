61fe1640fa46fa5149170dd5c69e684b8e561248
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-09-25 10:56:20 +0530

    akshay

--- src/main/java/com/shivshambhuwebapi/master/repo/GetOrderRepo.java
+++ src/main/java/com/shivshambhuwebapi/master/repo/GetOrderRepo.java
@@ -11,7 +11,7 @@ import com.shivshambhuwebapi.master.model.GetOrder;
 public interface GetOrderRepo extends JpaRepository<GetOrder, Integer> {
 
 	@Query(value = "SELECT t_order_header.*,"
-			+ " m_plant.plant_name,m_company.comp_name,m_customer.cust_name,m_customer.cust_mob_no,m_project.proj_name,m_project.address,t_cust_po_header.varchar1 as delivery, "
+			+ " m_plant.plant_name,m_company.comp_name,m_customer.cust_name,m_customer.cust_mob_no,m_project.proj_name,m_project.address,t_cust_po_header.varchar1 as delivery,t_cust_po_header.po_no "
 			+ " FROM t_order_header,m_plant,m_company,m_customer,m_project,t_cust_po_header "
 			+ " WHERE t_order_header.plant_id=m_plant.plant_id AND m_plant.company_id=m_company.company_id AND t_order_header.cust_id=m_customer.cust_id AND m_project.proj_id=t_order_header.proj_id"
 			+ " AND t_order_header.plant_id=:plantId AND t_order_header.cust_id=:custId AND "
@@ -19,7 +19,7 @@ public interface GetOrderRepo extends JpaRepository<GetOrder, Integer> {
 	List<GetOrder> getOrderBetweenDate(@Param("plantId") int plantId, @Param("custId") int custId,
 			@Param("fromDate") String fromDate, @Param("toDate") String toDate);
 
-	@Query(value = "SELECT t_order_header.*,t_cust_po_header.varchar1 as delivery,"
+	@Query(value = "SELECT t_order_header.*,t_cust_po_header.varchar1 as delivery,t_cust_po_header.po_no,"
 			+ " m_plant.plant_name,m_company.comp_name,m_customer.cust_name,m_customer.cust_mob_no,m_project.proj_name,m_project.address "
 			+ " FROM t_order_header,m_plant,m_company,m_customer,m_project,t_cust_po_header  "
 			+ " WHERE t_order_header.plant_id=m_plant.plant_id AND m_plant.company_id=m_company.company_id AND t_order_header.cust_id=m_customer.cust_id AND m_project.proj_id=t_order_header.proj_id"
@@ -28,7 +28,7 @@ public interface GetOrderRepo extends JpaRepository<GetOrder, Integer> {
 	List<GetOrder> getOrderBetweenDateCust(@Param("plantId") int plantId, @Param("fromDate") String fromDate,
 			@Param("toDate") String toDate);
 
-	@Query(value = "SELECT t_order_header.*,t_cust_po_header.varchar1 as delivery,"
+	@Query(value = "SELECT t_order_header.*,t_cust_po_header.varchar1 as delivery,t_cust_po_header.po_no,"
 			+ " m_plant.plant_name,m_company.comp_name,m_customer.cust_name,m_customer.cust_mob_no,m_project.proj_name,m_project.address "
 			+ " FROM t_order_header,m_plant,m_company,m_customer,m_project,t_cust_po_header "
 			+ " WHERE t_order_header.plant_id=m_plant.plant_id AND m_plant.company_id=m_company.company_id AND "
@@ -38,7 +38,7 @@ public interface GetOrderRepo extends JpaRepository<GetOrder, Integer> {
 
 	// 1
 
-	@Query(value = "SELECT t_order_header.*,"
+	@Query(value = "SELECT t_order_header.*,t_cust_po_header.po_no,"
 			+ " m_plant.plant_name,m_company.comp_name,m_customer.cust_name,m_customer.cust_mob_no,m_project.proj_name,m_project.address,t_cust_po_header.varchar1 as delivery "
 			+ " FROM t_order_header,m_plant,m_company,m_customer,m_project,t_cust_po_header "
 			+ " WHERE t_order_header.plant_id=m_plant.plant_id AND m_plant.company_id=m_company.company_id  AND  t_order_header.status IN (0,1) AND t_order_header.cust_id=m_customer.cust_id AND m_project.proj_id=t_order_header.proj_id"
@@ -48,7 +48,7 @@ public interface GetOrderRepo extends JpaRepository<GetOrder, Integer> {
 			@Param("fromDate") String fromDate, @Param("toDate") String toDate);
 
 //2
-	@Query(value = "SELECT t_order_header.*,"
+	@Query(value = "SELECT t_order_header.*,t_cust_po_header.po_no,"
 			+ " m_plant.plant_name,m_company.comp_name,m_customer.cust_name,m_customer.cust_mob_no,m_project.proj_name,m_project.address,t_cust_po_header.varchar1 as delivery "
 			+ " FROM t_order_header,m_plant,m_company,m_customer,m_project ,t_cust_po_header"
 			+ " WHERE t_order_header.plant_id=m_plant.plant_id AND m_plant.company_id=m_company.company_id  AND  t_order_header.status IN (0,1) AND t_order_header.cust_id=m_customer.cust_id AND m_project.proj_id=t_order_header.proj_id"
@@ -57,7 +57,7 @@ public interface GetOrderRepo extends JpaRepository<GetOrder, Integer> {
 	List<GetOrder> getPendingOrderBetweenDate(@Param("plantId") int plantId, @Param("fromDate") String fromDate,
 			@Param("toDate") String toDate);
 
-	@Query(value = "SELECT t_order_header.*,"
+	@Query(value = "SELECT t_order_header.*,t_cust_po_header.po_no,"
 			+ " m_plant.plant_name,m_company.comp_name,m_customer.cust_name,m_customer.cust_mob_no,m_project.proj_name,m_project.address,t_cust_po_header.varchar1 as delivery "
 			+ " FROM t_order_header,m_plant,m_company,m_customer,m_project,t_cust_po_header "
 			+ " WHERE t_order_header.plant_id=m_plant.plant_id AND m_plant.company_id=m_company.company_id AND t_order_header.cust_id=m_customer.cust_id AND m_project.proj_id=t_order_header.proj_id"
@@ -65,7 +65,7 @@ public interface GetOrderRepo extends JpaRepository<GetOrder, Integer> {
 	List<GetOrder> getOrderBetweenDateAll(@Param("fromDate") String fromDate, @Param("toDate") String toDate,
 			@Param("statusList") List<Integer> statusList);
 
-	@Query(value = "SELECT t_order_header.*,"
+	@Query(value = "SELECT t_order_header.*,t_cust_po_header.po_no,"
 			+ " m_plant.plant_name,m_company.comp_name,m_customer.cust_name,m_customer.cust_mob_no,m_project.proj_name,m_project.address,t_cust_po_header.varchar1 as delivery "
 			+ " FROM t_order_header,m_plant,m_company,m_customer,m_project,t_cust_po_header "
 			+ " WHERE t_order_header.plant_id=m_plant.plant_id AND m_plant.company_id=m_company.company_id AND t_order_header.cust_id=m_customer.cust_id AND m_project.proj_id=t_order_header.proj_id"
@@ -73,7 +73,7 @@ public interface GetOrderRepo extends JpaRepository<GetOrder, Integer> {
 	List<GetOrder> getOrderBetweenDateAndCust(@Param("fromDate") String fromDate, @Param("toDate") String toDate,
 			@Param("custId") int custId, @Param("statusList") List<Integer> statusList);
 
-	@Query(value = "SELECT t_order_header.*,"
+	@Query(value = "SELECT t_order_header.*,t_cust_po_header.po_no,"
 			+ " m_plant.plant_name,m_company.comp_name,m_customer.cust_name,m_customer.cust_mob_no,m_project.proj_name,m_project.address,t_cust_po_header.varchar1 as delivery "
 			+ " FROM t_order_header,m_plant,m_company,m_customer,m_project,t_cust_po_header "
 			+ " WHERE t_order_header.plant_id=m_plant.plant_id AND m_plant.company_id=m_company.company_id AND t_order_header.cust_id=m_customer.cust_id AND m_project.proj_id=t_order_header.proj_id"
@@ -81,7 +81,7 @@ public interface GetOrderRepo extends JpaRepository<GetOrder, Integer> {
 	List<GetOrder> getOrderBetweenDateAndPlant(@Param("fromDate") String fromDate, @Param("toDate") String toDate,
 			@Param("plantId") int plantId, @Param("statusList") List<Integer> statusList);
 
-	@Query(value = "SELECT t_order_header.*,"
+	@Query(value = "SELECT t_order_header.*,t_cust_po_header.po_no,"
 			+ " m_plant.plant_name,m_company.comp_name,m_customer.cust_name,m_customer.cust_mob_no,m_project.proj_name,m_project.address,t_cust_po_header.varchar1 as delivery "
 			+ " FROM t_order_header,m_plant,m_company,m_customer,m_project,t_cust_po_header "
 			+ " WHERE t_order_header.plant_id=m_plant.plant_id AND m_plant.company_id=m_company.company_id AND t_order_header.cust_id=m_customer.cust_id AND m_project.proj_id=t_order_header.proj_id"
