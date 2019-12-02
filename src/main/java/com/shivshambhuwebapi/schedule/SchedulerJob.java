a75fbca252fddd8b5dff17844ca6b476f6770939
Aaryatech <code@atsinfosoft.com>
2019-12-02 15:42:30 +0530

    //Anmol

--- src/main/java/com/shivshambhuwebapi/schedule/SchedulerJob.java
+++ src/main/java/com/shivshambhuwebapi/schedule/SchedulerJob.java
@@ -21,8 +21,8 @@ public class SchedulerJob {
 		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
 		System.err.println("CRON ---------------- "+sdf.format(Calendar.getInstance().getTimeInMillis()));
 		
-		String date="2019-01-25";
-		//String date=sdf.format(Calendar.getInstance().getTimeInMillis());
+		//String date="2019-01-25";
+		String date=sdf.format(Calendar.getInstance().getTimeInMillis());
 		int res=orderHeaderRepo.updateStatusDateLessThanCurrent(2, date);
 		System.err.println("RESULT ---------------- "+res);
 		
